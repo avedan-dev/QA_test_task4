6fe1aedd5038b10777bcd2379408f3fd72f13705
avedan <damir4iki1@gmail.com>
2021-02-28 15:56:03 +0300

    clean_up fix, comments added

--- test_case_2.py
+++ test_case_2.py
@@ -14,32 +14,35 @@ class TestCase2:
                     total_mem = int(sline[1])
         assert total_mem // 1024 ** 2 >= n, f'Объем оперативной памяти меньше {n} Гб'
 
+    # Удаляет файлы созданные во время теста
     def clean_up(self):
-        try:
-            os.remove('test')
-        except FileNotFoundError:
-            print(' Файл не найден')
+        for i in os.listdir():
+            if i in Test_case_2.FILE_WRONG_NAMES or i == 'test':
+                os.remove(i)
         assert 'test' not in os.listdir(), 'В директории остался файл test'
 
+    # Выполняется перед каждым тестом
     @pytest.fixture(scope="function", autouse=True, params=Test_case_2.MEM_SIZE, ids=Test_case_2.IDS)
-    def setup(self, request):
+    def run_test(self, request):
         self.prep(request.param)
         yield
         self.clean_up()
 
+    # Создает файл test необходимой длинны
     def test_create_file(self):
         with open('test', 'wb') as f:
             f.write(os.urandom(1000))
         assert os.path.getsize('test') == 1000
 
+    # Xfail тесты, которые создают файлы с неверным названием
     @pytest.mark.xfail(reason='Намеренно создали файл с другим названием')
     @pytest.mark.parametrize("name", Test_case_2.FILE_WRONG_NAMES)
     def test_create_file_failed(self, name):
         with open(name, 'wb') as f:
             f.write(os.urandom(1000))
-        os.remove(name)
         assert os.path.getsize('test') == 1000
 
+    # Xfail тесты, которые создают файлы с неверного размера
     @pytest.mark.xfail(reason='Намеренно создали файл другого размера')
     @pytest.mark.parametrize("size", Test_case_2.FILE_WRONG_SIZE)
     def test_create_wrong_size(self, size):
